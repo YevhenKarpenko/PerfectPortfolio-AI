ed37cacde2ee5c64b107e4a4c5510c25c72f9947
Yevhen <yevhen@Air-Yevhen.fritz.box>
2024-04-09 15:16:06 +0200

    master

--- src/components/useGenerateCV.js
+++ src/components/useGenerateCV.js
@@ -6,9 +6,26 @@ const useGenerateCV = () => {
     const [error, setError] = useState(null);
     const [cvText, setCvText] = useState('');
 
+
+    const url = 'https://api.openai.com/v1/chat/completions';
+
+  
+
     const generateCVContent = async (userInfo, experienceArray, educationArray) => {
         setLoading(true);
         setError(null);
+
+
+      
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          };
+    
+         
+
+       
 
         // Constructing the prompt
         let prompt = `Create a professional CV for the following details:\nName: ${userInfo.name}\nPosition: ${userInfo.position}\nPhone: ${userInfo.phoneNumber}\nEmail: ${userInfo.email}\nLinkedIn: ${userInfo.linkedinURL}\nPortfolio: ${userInfo.portfolioURL}\nLocation: ${userInfo.location}\nDescription: ${userInfo.description}\n\nExperience:\n`;
@@ -22,23 +39,25 @@ const useGenerateCV = () => {
             prompt += `${edu.degree} at ${edu.schoolName}, Location: ${edu.location}, GPA:${edu.gpa} Graduation date:${edu.graduationDate}\n`;
         });
 
-        try {
-            const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-                model: "gpt-4", // Update model as necessary
-                prompt: prompt,
-                temperature: 0.5,
-                max_tokens: 1024,
-                top_p: 1.0,
-                frequency_penalty: 0.0,
-                presence_penalty: 0.0
-            }, {
-                headers: {
-                    'Authorization': `Bearer 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`,
-                    'Content-Type': 'application/json'
+        const data = {
+            model: "gpt-3.5-turbo-0125",
+             messages: [
+                {
+                  role: "system",
+                  content: "You are a helpful assistant that created a perfectly CV baced on the input data"
+                },
+                {
+                  role: "user",
+                  content: prompt
                 }
-            });
+              ]
+          };
+       
 
-            setCvText(response.data.choices[0].text);
+        try {
+            const response = await axios.post(url, data, { headers });
+            setCvText(response.data.choices[0].message.content);
+
         } catch (error) {
             console.error('Error generating CV:', error);
             setError(error);
